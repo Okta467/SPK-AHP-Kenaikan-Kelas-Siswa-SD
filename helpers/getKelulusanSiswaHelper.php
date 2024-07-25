745375e0d4ac258c6257de8c991e8a87f93969a0
okta467 <oktaalfiansyah@gmail.com>
2024-07-25 20:37:04 +0700

    Update: add $kkm and $jmlMaksDiBawahKKM in helper functiongetKelulusanSiswa

--- helpers/getKelulusanSiswaHelper.php
+++ helpers/getKelulusanSiswaHelper.php
@@ -6,11 +6,16 @@
      * 
      * @param null|array<mixed> $siswa contains nilai per category (kriteriaNilai), e.g. 'kehadiran' => 89, 'uas' => 90
      * @param null|array<string> $kriteriaNilai e.g. ['kehadiran', 'uas', ...]
+     * @param int $kkm range from 0-100 `Kriteria Ketuntasan Minimal` or minimum criteria score need to able to `Lulus`,
+     * @param null|int
+     * $jmlMaksDiBawahKKM maximum count for criteria score less than $kkm, if more than maximum, then `keterangan_kelulusan` is `Tidak Lulus`.
+     * If not defined, this parameter use count($kriteriaNilai) instead
      * @return array contains message (success/error), data (kode alternatif, keterangan kelulusan)
      */
-    function getKelulusanSiswa($siswa = null, $kriteriaNilai = null) {
+    function getKelulusanSiswa($siswa = null, $kriteriaNilai = null, $jmlMaksDiBawahKKM = null, $kkm = 70) {
         $jmlSiswa = count($siswa);
         $jmlKriteriaNilai = count($kriteriaNilai);
+        $jmlMaksDiBawahKKM = $jmlMaksDiBawahKKM ?? $jmlKriteriaNilai;
         
         if (!$siswa || !$kriteriaNilai) {
             return [
@@ -34,12 +39,12 @@
             for ($j = 0; $j < $jmlKriteriaNilai; $j++) {
                 $currentNilai = $siswa[$i][$kriteriaNilai[$j]];                
 
-                if ($currentNilai < 70) {
+                if ($currentNilai < $kkm) {
                     $jmlDiBawahKKM++;
                 }
             }
 
-            if ($jmlDiBawahKKM > 4) {
+            if ($jmlDiBawahKKM >= $jmlMaksDiBawahKKM) {
                 $data['data'][$i]['keterangan_kelulusan'] = 'Tidak Lulus';
             }
         }
